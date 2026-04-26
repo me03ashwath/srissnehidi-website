60344f20de752e9dbf777d6d306fb7e2c686686e
Ashwath S <99113851+me03ashwath@users.noreply.github.com>
2026-04-26 20:38:56 +0530

    Added icons, slidshow in home page, remove unnecessary padding.

--- core/constants.py
+++ core/constants.py
@@ -8,7 +8,7 @@ SITE = {
     ),
     "phone": "+91 9092518668",
     "whatsapp": "919092518668",
-    "address": "2rd floor, Door no.33, Plot no.5/7, Sarathy Nagar 3rd Street, Puzhuthivakkam, Chennai-600091",
+    "address": "2rd Floor, Door no.33, Plot no.5/7, Sarathy Nagar 3rd Street, Puzhuthivakkam, Chennai-600091",
     "instagram": "https://instagram.com/srissnehidi",
     "facebook": "https://www.facebook.com/people/Sris-Snehidi-Fashion-Institute/100063580346645/#",
     "youtube": "https://www.youtube.com/@srissnehidifashioninstitut9049",
