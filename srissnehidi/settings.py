a65e38f804c986569563af10f242a559ea404ab6
Ashwath S <99113851+me03ashwath@users.noreply.github.com>
2026-05-09 11:04:31 +0530

    Added pulsating whatsapp logo to base template, added SEO

--- srissnehidi/settings.py
+++ srissnehidi/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     # Third-party
     'cloudinary',
     'cloudinary_storage',
